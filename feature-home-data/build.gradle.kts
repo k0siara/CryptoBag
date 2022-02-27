50610a8de2ebc53ed8c692ef9bb02176dff9b967
k0siara <patryk.kosieradzki@gmail.com>
2022-02-27 23:40:52 +0100

    Fixes common-remote

--- feature-home-data/build.gradle.kts
+++ feature-home-data/build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    id("kotlin-kapt")
+}
+
+dependencies {
+    implementation(project(":utils"))
+    implementation(project(":common-remote"))
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
